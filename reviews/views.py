20b33cbad3f97859ff30a35fc7d4f7b5c2d62528
Mārcis <marcisreb@gmail.com>
2024-03-28 11:11:36 +0200

    model and first migration

--- reviews/views.py
+++ reviews/views.py
@@ -5,3 +5,8 @@ def index(request):
     name = "marcis"
     context = {"name": name}
     return render(request, 'base.html', context)
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    search_context = {"search_text": search_text}
+    return render(request, 'search-results.html', search_context)
